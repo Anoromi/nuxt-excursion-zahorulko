072f99c2161059e456bf3dc8677c1eaf68f9bb58
Anoromi <anoromi@DESKTOP-1SVA5E4>
2023-09-05 14:27:04 +0300

    Add google verification

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,10 @@ useHead({
       name: 'description',
       content: 'Anci, your new experiences',
     },
+    {
+      name: "google-site-verification",
+      content: "I7BNdHaXymkS_z9hi36i6CsEF95lhGIWPgVRhApwMns"
+    }
   ],
 })
 
@@ -30,16 +34,10 @@ function scrollGallery(x: number) {
 </script>
 
 <template>
-  <article
-    class="min-h-full w-full bg-surface3-light dark:bg-surface3-dark"
-  >
-    <section
-      class="flex h-screen flex-col justify-center px-4 text-center sm:px-[15%]"
-    >
+  <article class="min-h-full w-full bg-surface3-light dark:bg-surface3-dark">
+    <section class="flex h-screen flex-col justify-center px-4 text-center sm:px-[15%]">
       <TextTitle class="text-6xl">Anci</TextTitle>
-      <em
-        class="font-serif text-3xl font-bold not-italic text-primary-light dark:text-primary-dark"
-      >
+      <em class="font-serif text-3xl font-bold not-italic text-primary-light dark:text-primary-dark">
         Your experiences matter
       </em>
     </section>
@@ -49,85 +47,48 @@ function scrollGallery(x: number) {
         Our selection
       </h2>
       <div class="relative overflow-x-hidden rounded-3xl">
-        <button
-          class="highlight-none group absolute left-0 hidden h-full items-center sm:flex"
-          aria-label="scroll left"
-          type="button"
-          @click="scrollGallery(-scrollSpeed)"
-        >
-          <Icon
-            name="ic:round-chevron-left"
-            size="50px"
-            class="group-hover:bg-primary ml-5 h-[30%] rounded-full text-transparent outline-2 outline-primary-container-light transition-colors duration-500 group-hover:outline dark:outline-primary-container-dark"
-          >
+        <button class="highlight-none group absolute left-0 hidden h-full items-center sm:flex" aria-label="scroll left"
+          type="button" @click="scrollGallery(-scrollSpeed)">
+          <Icon name="ic:round-chevron-left" size="50px"
+            class="group-hover:bg-primary ml-5 h-[30%] rounded-full text-transparent outline-2 outline-primary-container-light transition-colors duration-500 group-hover:outline dark:outline-primary-container-dark">
           </Icon>
         </button>
-        <button
-          class="highlight-none group absolute right-0 hidden h-full items-center sm:flex"
-          aria-label="scroll right"
-          type="button"
-          @click="scrollGallery(scrollSpeed)"
-        >
-          <Icon
-            name="ic:round-chevron-right"
-            size="50px"
-            class="group-hover:bg-primary mr-5 h-[30%] rounded-full text-transparent outline-2 outline-primary-container-light transition-colors duration-500 group-hover:outline dark:outline-primary-container-dark"
-          >
+        <button class="highlight-none group absolute right-0 hidden h-full items-center sm:flex" aria-label="scroll right"
+          type="button" @click="scrollGallery(scrollSpeed)">
+          <Icon name="ic:round-chevron-right" size="50px"
+            class="group-hover:bg-primary mr-5 h-[30%] rounded-full text-transparent outline-2 outline-primary-container-light transition-colors duration-500 group-hover:outline dark:outline-primary-container-dark">
           </Icon>
         </button>
-        <div
-          class="my-11 flex h-[400px] w-full items-center gap-x-3 overflow-x-auto px-3 sm:overflow-x-hidden"
-          v-if="!loadingGallery"
-          ref="galleryDiv"
-        >
-          <img
-            class="block max-h-[100%] max-w-[calc(70vw)] rounded-3xl"
-            v-for="{
-              photo: {
-                id: pictureId,
-                originalHeight,
-                originalWidth,
-              },
-              id: itemId,
-              title,
-            } in gallery?.slice(0, 3)"
-            :key="itemId"
-            :src="
-              imageUrl(
-                pictureId,
-                originalWidth,
-                originalHeight,
-              )
-            "
-            :alt="title"
-            :width="originalWidth"
-            :height="originalHeight"
-            loading="eager"
-          />
-          <img
-            class="block max-h-[100%] max-w-[calc(70vw)] rounded-3xl"
-            v-for="{
-              photo: {
-                id: pictureId,
-                originalHeight,
-                originalWidth,
-              },
-              id: itemId,
-              title,
-            } in gallery?.slice(3)"
-            :key="itemId"
-            :src="
-              imageUrl(
-                pictureId,
-                originalWidth,
-                originalHeight,
-              )
-            "
-            :alt="title"
-            :width="originalWidth"
-            :height="originalHeight"
-            loading="lazy"
-          />
+        <div class="my-11 flex h-[400px] w-full items-center gap-x-3 overflow-x-auto px-3 sm:overflow-x-hidden"
+          v-if="!loadingGallery" ref="galleryDiv">
+          <img class="block max-h-[100%] max-w-[calc(70vw)] rounded-3xl" v-for="{
+            photo: {
+              id: pictureId,
+              originalHeight,
+              originalWidth,
+            },
+            id: itemId,
+            title,
+          } in gallery?.slice(0, 3)" :key="itemId" :src="imageUrl(
+  pictureId,
+  originalWidth,
+  originalHeight,
+)
+  " :alt="title" :width="originalWidth" :height="originalHeight" loading="eager" />
+          <img class="block max-h-[100%] max-w-[calc(70vw)] rounded-3xl" v-for="{
+            photo: {
+              id: pictureId,
+              originalHeight,
+              originalWidth,
+            },
+            id: itemId,
+            title,
+          } in gallery?.slice(3)" :key="itemId" :src="imageUrl(
+  pictureId,
+  originalWidth,
+  originalHeight,
+)
+  " :alt="title" :width="originalWidth" :height="originalHeight" loading="lazy" />
         </div>
       </div>
     </section>
